3ab5eb79ac80621dc20deb402e93e025643c7b06
cHacHaem <113141138+cHacHaem@users.noreply.github.com>
2022-09-10 09:43:29 -0400

    Update sketch.js

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400, 400);
+}
+function draw() {
+  background(220);
+}
+
+function printDavesLocation() {
+  console.log("Dave is climbing a tree in the backyard!");
+}
